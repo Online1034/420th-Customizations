88c20f3a01a0d4ff966f7d6ec00201af8b121ab2
online1034 <iaydrwg200997@qq.com>
2026-09-09 21:26:52 +0800

    increse capacitiy of vests from EF CDLC (request from DM)

--- addons/magwells_ef/config.cpp
+++ addons/magwells_ef/config.cpp
@@ -57,4 +57,53 @@ class CfgWeapons {
             };
         };
     };
+	class Vest_NoCamo_Base: ItemCore {
+		class ItemInfo;
+	};
+	class EF_V_AAV_Coy: Vest_NoCamo_Base {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //40
+        };
+	};
+	class EF_V_AAV_Diver_Coy: EF_V_AAV_Coy {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //80
+        };
+	};
+	class EF_V_AAV_Diver_NoReb_Coy: EF_V_AAV_Coy {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //80
+        };
+	};    
+	class EF_V_AAV_Rifleman_Coy: EF_V_AAV_Coy {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //140
+        };
+	};
+	class EF_V_AAV_Sailor_Coy: EF_V_AAV_Coy {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //140
+        };
+	};
+	class EF_V_AAV_Scout_Coy: EF_V_AAV_Coy {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //140
+        };
+	};
+	class EF_V_AAV_Support_Coy: EF_V_AAV_Coy {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //140
+        };
+	};    
+	class EF_V_AAV_TL_Coy: EF_V_AAV_Coy {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //140
+        };
+	};
+	class EF_V_CCR_Rifleman_Coy: Vest_NoCamo_Base {
+		class ItemInfo: ItemInfo {
+            containerClass = "Supply300"; //140
+        };
+	};         
 };
+
