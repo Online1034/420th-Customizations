7d1106ea20895b23be7b4dc6637842cf61c93c73
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-09-10 08:26:37 -0400

    feat: depend on all vanilla addons in fdelta_main

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -7,6 +7,17 @@ class CfgPatches {
         requiredVersion = 2.22;
         requiredAddons[] = {
             "A3_Data_F_Decade_Loadorder",
+            // All vanilla addons should be added here as dependencies.
+            // External mods can depend on fdelta_main to transitively depend on
+            // these addons.
+            "fdelta_backpacks",
+            "fdelta_capacity",
+            "fdelta_guidance",
+            "fdelta_he_rebalance",
+            "fdelta_optics_nvgs",
+            "fdelta_optics_zoom",
+            "fdelta_pylons",
+            "fdelta_viv_transport",
         };
         skipWhenMissingDependencies = 0;
 
