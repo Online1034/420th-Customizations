8b8e26afdf3407d08c634426fbccac5f0b1a73b3
thegamecracks <61257169+thegamecracks@users.noreply.github.com>
2026-09-10 07:09:57 -0400

    refactor: add redundant decade loadorder for consistency

--- addons/viv_transport/config.cpp
+++ addons/viv_transport/config.cpp
@@ -17,6 +17,7 @@ class CfgPatches {
 
         requiredVersion = 2.22;
         requiredAddons[] = {
+            "A3_Data_F_Decade_Loadorder",
             "fdelta_main",
         };
         skipWhenMissingDependencies = 1;
